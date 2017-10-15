3e22d013a430636f39563d80bdec7e4982b0e0c7
Jefferson Matheus <jefferson227@gmail.com>
2017-10-15 08:11:25 -0300

    Add first connection with ingresso.com api

--- index.js
+++ index.js
@@ -2,38 +2,28 @@ var express = require('express');
 var cheerio = require('cheerio');
 var request = require('request');
 var app = express();
+var baseUrl = 'https://api-content.ingresso.com/v0';
+var partnership = '/partnership/0';
 
 app.get('/', function (req, res) {
   var url = 'http://www.movietickets.com/theaters/detail/id/ti-6166';
 
   request(url, function (error, response, html) {
     if (!error) {
-      var $ = cheerio.load(html);
-      var movie = $('row.theater-detail-movie');
-      var name = $(movie).find('.theater-detail-movie-title').html();
-      var runtime = $(movie).find('.theater-detail-movie-rating-runtime').html();
-      var str = 'Name: ' + name + ', runtime: ' + runtime;
-      str = '';
-
-      $('h4.theater-detail-movie-title').each(function (index, element) {
-        var str2 = '';
-        var showtimeAttribute = $(element)
-          .find('.showtime-attribute');
-
-        $(showtimeAttribute).each(function (i, el) {
-          str2 += $(el).html() + ' ';
-        });
-
-        str += $(element).html() + '<br>' + str2 + '<br><br>';
-      });
-
-      // $(movie).each(function (index, element) {
-      //   var name = $(element).find('.theater-detail-movie-title').html();
-      //   var runtime = $(element).find('.theater-detail-movie-rating-runtime').html();
-      //   str += 'name: ' + name + ', runtime: ' + runtime + '<br>';
-      // });
-
       res.send(str);
+    }
+    else {
+      res.send(error);
+    }
+  });
+});
+
+app.get('/events/city/:id', function (req, res) {
+  var url = baseUrl + '/events/city/' + req.params.id + partnership;
+
+  request(url, function (error, response, html) {
+    if (!error) {
+      res.send(html);
     }
     else {
       res.send(error);
