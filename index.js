0efd19b1610e7941644a575947f07b4cd2c5b3d5
Jefferson Matheus <jefferson227@gmail.com>
2017-10-21 08:51:30 -0300

    Add theaters by movie endpoint

--- index.js
+++ index.js
@@ -132,6 +132,88 @@ app.get('/movies', function (req, res) {
   });
 });
 
+app.get('/movie/:movieId/sessions', function (req, res) {
+  // First: Getting all the movies
+  var url      = 'http://www.movietickets.com/movies';
+  var movies   = [];
+  var theaters = [];
+
+  request(url, function (error, response, html) {
+    if (!error) {
+      var $      = cheerio.load(html);
+      var result = '';
+
+      $('script').each(function (i, el) {
+        var strJson = $(el).html();
+
+        if (strJson.indexOf('MovielandingJsonObject') > -1) {
+          result = strJson.replace('var MovielandingJsonObject = ', '').replace(';', '');
+        }
+      });
+
+      var jsonMovies = JSON.parse(result).MoviesNowPlaying
+        .map(function (item) {
+          var genres = item.Genre
+            .map(function (genre) {
+              return genre.Attributes.Name;
+            });
+
+          var id = item.LinkUrl.split('/')[6]
+            .split('-')[1];
+
+          return {
+            id            : id,
+            title         : item.Details.Name,
+            duration      : item.Details.RuntimeInMinutes,
+            contentRating : item.Details.Rating,
+            images        : [{
+              url: item.Details.PosterUrl.replace('{width}', '800').replace('{height}', '600')
+            }],
+            genres        : genres
+          }
+        });
+
+      movies = JSON.parse(jsonMovies)
+        .filter(function (movieFilter) { return movieFilter.id = req.params.movieId });
+
+      // Second: Getting all the theaters by city
+      if (movies.length) {
+        var city = 'ottawa';
+        var url  = 'https://www.movietickets.com/search?indexCatalogue=www-site&searchQuery=' + city + '&wordsMode=AllWords';
+
+        request(url, function (error, response, html) {
+          if (!error) {
+            var $ = cheerio.load(html);
+
+            $('.search-grouping').each(function (i, el) {
+              var theater = {
+                name    : $(el).find('.search-item a').html(),
+                address : $(el).find('.search-descript').html(),
+                link    : $(el).find('.search-link').html()
+              };
+
+              theaters.push(theater);
+            });
+          }
+          else {
+            throw 'Error on getting theaters';
+          }
+        });
+      }
+
+      // Third: Getting all the sessions on theaters by movie
+      if (theaters.length) {
+
+      }
+
+      res.send(jsonMovies);
+    }
+    else {
+      res.send(error);
+    }
+  });
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
