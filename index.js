b7289401af30d54756fc5f9d4b3eb559c7785e14
Jefferson Matheus <jefferson227@gmail.com>
2017-11-11 10:27:30 -0300

    Remove unnecessary code

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var express = require('express');
-var cheerio = require('cheerio');
-var request = require('request');
-var requestPromise = require('request-promise');
 var utilsModule = require('./utils');
 var utils = utilsModule();
 var app = express();
 var baseUrl = 'https://www.tribute.ca';
-var partnership = '/partnership/0';
 var port = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
@@ -16,58 +12,6 @@ app.use(function (req, res, next) {
 
 app.get('/', function (req, res) {
   res.send('Hello this is the busca-cinema-backend :)');
-});
-
-app.get('/events/city/:id', function (req, res) {
-  var url = baseUrl + '/events/city/' + req.params.id + partnership;
-
-  request(url, function (error, response, html) {
-    if (!error) {
-      res.send(html || []);
-    }
-    else {
-      res.send(error);
-    }
-  });
-});
-
-app.get('/events/:id', function (req, res) {
-  var url = baseUrl + '/events/' + req.params.id + partnership;
-
-  request(url, function (error, response, html) {
-    if (!error) {
-      res.send(html || []);
-    }
-    else {
-      res.send(error);
-    }
-  });
-});
-
-app.get('/events/:id', function (req, res) {
-  var url = baseUrl + '/events/' + req.params.id + partnership;
-
-  request(url, function (error, response, html) {
-    if (!error) {
-      res.send(html || []);
-    }
-    else {
-      res.send(error);
-    }
-  });
-});
-
-app.get('/sessions/city/:cityId/event/:movieId/date/:date', function (req, res) {
-  var url = baseUrl + '/sessions/city/' + req.params.cityId + '/event/' + req.params.movieId + partnership + '?date=' + req.params.date;
-
-  request(url, function (error, response, html) {
-    if (!error) {
-      res.send(html || []);
-    }
-    else {
-      res.send(error);
-    }
-  });
 });
 
 app.get('/movies', function (req, res) {
